0d490dac4ada25970e520f86f318ac04aa823b4f
Lars van der Ham <lars@vanderham.email>
2020-02-24 22:25:58 +0100

    Fix crash when trying to len a generator Start on CalendarEventDevice implementation

--- custom_components/etesync_calendar/calendar.py
+++ custom_components/etesync_calendar/calendar.py
@@ -2,12 +2,17 @@ import voluptuous as vol
 import logging
 from etesync import Authenticator, EteSync
 
-from homeassistant.components.calendar import PLATFORM_SCHEMA, CalendarEventDevice
+from homeassistant.components.calendar import (
+    ENTITY_ID_FORMAT,
+    PLATFORM_SCHEMA,
+    CalendarEventDevice
+)
+
 
 from homeassistant.const import (
     CONF_PASSWORD,
     CONF_URL,
-    CONF_EMAIL,
+    CONF_USERNAME,
     CONF_VERIFY_SSL,
 )
 import homeassistant.helpers.config_validation as cv
@@ -18,15 +23,16 @@ CONF_ENCRYPTION_PASSWORD = "encryption_password"
 
 DOMAIN = "etesync_calendar"
 
+CALENDAR_ITEM_TYPE = "CALENDAR"
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        # pylint: disable=no-value-for-parameter
         vol.Required(CONF_URL): vol.Url(),
-        vol.Required(CONF_EMAIL): cv.str,
+        vol.Required(CONF_USERNAME): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
         vol.Required(CONF_ENCRYPTION_PASSWORD): cv.string,
 
-        vol.Optional(CONF_VERIFY_SSL, default=True): cv.boolean,
+        # vol.Optional(CONF_VERIFY_SSL, default=True): cv.boolean,
     }
 )
 
@@ -35,14 +41,14 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup_platform(hass, config, add_entities, disc_info=None):
     url = config[CONF_URL]
-    email = config[CONF_EMAIL]
+    username = config[CONF_USERNAME]
     password = config[CONF_PASSWORD]
     encryption_password = config[CONF_ENCRYPTION_PASSWORD]
 
-    # Token should be saved intead of requested every time
-    auth_token = Authenticator(url).get_auth_token(email, password)
+    # Token should be saved instead of requested every time
+    auth_token = Authenticator(url).get_auth_token(username, password)
 
-    etesync = EteSync(email, auth_token, remote=url)
+    etesync = EteSync(username, auth_token, remote=url)
     _LOGGER.info("Deriving key")
     # Very slow operation, should probably be securely cached
     etesync.derive_key(encryption_password)
@@ -52,6 +58,29 @@ def setup_platform(hass, config, add_entities, disc_info=None):
 
     items = etesync.list()
 
-    _LOGGER.info("Synced {} items", len(items))
+    devices = []
 
-    pass
+    for item in items:
+        # Filter task list / address book's
+        if item.info['type'] == CALENDAR_ITEM_TYPE:
+            entity_id = generate_entity_id(ENTITY_ID_FORMAT, item.info['displayName'].lower(), hass=hass)
+            device = EteSyncCalendarEventDevice(item, entity_id)
+            devices.append(device)
+
+    add_entities(devices, True)
+
+
+class EteSyncCalendarEventDevice(CalendarEventDevice):
+    """A device for a single etesync calendar."""
+
+    def __init__(self, calendar, entity_id):
+        self._calendar = calendar
+        self._name = calendar.info['displayName']
+        self._entity_id = entity_id
+
+    @property
+    def event(self):
+        return None
+
+    async def async_get_events(self, hass, start_date, end_date):
+        pass
